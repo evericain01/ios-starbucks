42b0d40281849229d300fd27a2d077d9a3d101ed
evericain01 <evericain01@hotmail.com>
2022-09-29 22:49:16 -0400

    Added table view

--- Starbucks/Home/HomeViewController.swift
+++ Starbucks/Home/HomeViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 class HomeViewController: StarBucksViewController {
 
     let headerView = HomeHeaderView()
+    var tableView = UITableView()
+    
+    let cellId = "cellId"
+    let tiles = [
+                "Star balance",
+                "Bonus stars",
+                "Try these",
+                "Welcome back",
+                "Uplifting"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        setupNavBar()
         setTabBarImage(imageName: "house.fill", title: "Home")
-        
+        setupTableView()
         style()
         layout()
     }
@@ -39,16 +49,67 @@ extension HomeViewController {
     // MARK: - Style
     func style() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.backgroundColor = .systemPink
+        tableView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     // MARK: - Layout
     func layout() {
         view.addSubview(headerView)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+// MARK: - TableView
+extension HomeViewController: UITableViewDataSource {
+    
+    // Setting up the table view
+    func setupTableView() {
+        
+        // Set the HomeViewController as the datasource and delegate of the tableview
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
+    }
+    
+    // Customizing that specific cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = tiles[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        
+        return cell
+    }
+    
+    // Number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tiles.count
+    }
+    
+
+}
+
+// MARK: - Animating scrollView
+extension HomeViewController: UITableViewDelegate {
+    
+    // Giving a nice thick height for each row in the table view
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
     }
 }
