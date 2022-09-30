ab1083b779aa813d7d97e2684c99d0b7b0c9e2ea
evericain01 <evericain01@hotmail.com>
2022-09-29 23:04:15 -0400

    Animated the scroll in the table view

--- Starbucks/Home/HomeViewController.swift
+++ Starbucks/Home/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class HomeViewController: StarBucksViewController {
 
     let headerView = HomeHeaderView()
+    var headerViewTopConstraint: NSLayoutConstraint?
     var tableView = UITableView()
     
     let cellId = "cellId"
@@ -58,8 +59,11 @@ extension HomeViewController {
         view.addSubview(headerView)
         view.addSubview(tableView)
         
+        // Constraint for top header view
+        headerViewTopConstraint = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerViewTopConstraint!,
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
@@ -111,5 +115,23 @@ extension HomeViewController: UITableViewDelegate {
     // Giving a nice thick height for each row in the table view
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 300
+    }
+    
+    // When scrolling
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = scrollView.contentOffset.y
+     
+        let swipingDown = y <= 0
+        let shouldSnap = y > 30
+        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
+        }
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: [], animations: {
+            self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
+            self.view.layoutIfNeeded()
+        })
     }
 }
